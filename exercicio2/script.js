831b0d72f535bd840f260dc6ecd7c76730960607
Aureana <111321782+Aureana@users.noreply.github.com>
2022-08-29 16:33:24 -0300

    adicionando-informacoes

---
+++ exercicio2/script.js
@@ -0,0 +1,17 @@
+let pergunta1 = "Você gosta de bala?"
+let bala= prompt(pergunta1)
+console.log (bala)
+   //para que o texto das perguntas também sejam guardadas em variáveis
+
+let pergunta2 = "voce esta usando blusa verde hoje?"
+let blusa = prompt(pergunta2)
+console.log (blusa)
+
+let pergunta3 = "Está chovendo hoje?"
+let chuva = prompt(pergunta3)
+console.log (chuva)
+
+console.log(`Você gosta de bala ${bala}, e você está usando blusa verde hoje ${blusa}, e ${chuva} está chuvendo hoje.`)
+
+
+
